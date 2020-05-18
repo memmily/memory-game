702a3be4e90dc794590b58681c2f0bba031b1448
mayara <may.emmily@gmail.com>
2020-05-18 22:06:54 +0200

    Testing  functionalities

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,32 @@
-new Vue({
-    el: '#container',
-    data: {
-      
-    }
-  })
+
+
+//card turns front face with click
+
+//card turns back face when other card is clicked
+
+const innerCards = document.querySelector(".game-card");
+const frontFace = document.getElementsByClassName("front");
+const backFace = document.getElementsByClassName("back");
+
+
+
+innerCards.addEventListener("click", flipCard);
+
+function flipCard() {
+  let flippedCard;
+  if(backFace) {
+    flippedCard = frontFace;
+  }
+  return flippedCard
+}
+  
+
+
+
+
+
+
+
+
+
+
