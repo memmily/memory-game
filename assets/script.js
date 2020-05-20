5798da87b4b027a0ac58ec48bae4b6da78a35a48
mayara <may.emmily@gmail.com>
2020-05-20 18:53:37 +0200

    cards turn back to the original position

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+
 //card turns front face with click
 
 let innerCards = document.querySelectorAll(".game-card");
@@ -5,11 +6,21 @@ let innerCards = document.querySelectorAll(".game-card");
 function flipCard(card) {
   card.classList.toggle("active");
 }
-  
+
 innerCards.forEach(function (currentCard){
   currentCard.addEventListener('click', function(event) {
-    flipCard(event.currentTarget);
+    let target = event.currentTarget;
+    flipCard(target);
+    //card turns back to the original position after a while
+    setTimeout(function(){
+      flipCard(target);
+    }, 3000);
+    
   });
 });
 
+//TODO:
+
 //card turns back face when other card is clicked
+
+//Study function bind and hoisting
