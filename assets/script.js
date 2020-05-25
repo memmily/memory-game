240375b6fe95c38a4c3c72df0576b5288d750823
mayara <may.emmily@gmail.com>
2020-05-25 16:29:34 +0200

    New features on cards states

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,47 @@
-
 //card turns front face with click
 
 const innerCards = document.querySelectorAll(".game-card");
+
+var cardOne = null;
+var cardTwo = null;
+var timerId = null;
+
+
 
 function flipCard(card) {
   card.classList.toggle("active");
 }
 
+function setClickedCard(card) {
+  if(cardOne===null) {
+    cardOne = card;
+  } else if(cardTwo===null) {
+    cardTwo = card;
+  } else {
+    flipCard(cardOne);
+    flipCard(cardTwo);
+    cardOne = card;
+    cardTwo = null;
+  }
+}
+
 innerCards.forEach(function (currentCard){
   currentCard.addEventListener('click', function(event) {
-    let target = event.currentTarget;
-    flipCard(target);
+    card = event.currentTarget;
+    flipCard(card);
 
-    //Turn back cards
-    turnBack(document.querySelectorAll(".active"));
-    
+    setClickedCard(card);
+
+    //if you click the same card already clicked, do nothing
+
+    //Turns two cards face back after click
+
+ //Turn back cards
+    // turnBack(document.querySelectorAll(".active"));
+
   });
 });
-
-
-// Turns two cards face back after click
-
+    
 function turnBack(activeCards) {
     if(activeCards.length>=2) {
       setTimeout(function(cards){
@@ -33,8 +54,30 @@ function turnBack(activeCards) {
 
 
 
+//Game states
+
+// estado inicial - todas as cartas devem estar viradas para baixo
+// clicked_card_1 deve ser null
+// clicked_card_2 deve ser null
+// timerId deve ser null
+
+// ao clicar em uma nova carta ...
+
+// ao clicar em outra nova carta ...
+
+// durante o timeout ...
+
+// ao final do timeout ...
+
+
+// o jogo acaba quando ...?
+
+
 //TODO:
 
 //Block when person tries to turn a third card
 
+//save currentcard
+
 //Study function bind, parameters and hoisting
+
