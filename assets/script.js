a79d69866296df13e5faf65a152057f0ea4626e9
mayara <may.emmily@gmail.com>
2020-06-12 10:31:30 +0200

    Rebuilding project with JS

--- assets/script.js
+++ assets/script.js
@@ -1,137 +1,30 @@
-//card turns front face with click
-
-const innerCards = document.querySelectorAll(".game-card");
-
-var cardOne = null;
-var cardTwo = null;
-var timerId = null;
-
-
-
-function flipCard(card) {
-  card.classList.toggle("active");
-
-}
-
-function setFlipStates(card) {
-
-  if(cardOne===card || cardTwo===card) {
-    console.log("This card was already clicked");
-    return
-  }
-
-  if(card.classList.contains("matched")) {
-    console.log("This card was already matched");
-    return
-
-  }
-
-  if(cardOne===null) {
-    cardOne = card;
-    flipCard(cardOne);
-    console.log({cardOne});
-  } else if(cardTwo===null) {
-    cardTwo = card;
-    flipCard(cardTwo);
-    console.log({cardTwo});
-  } else {
-    flipCard(cardOne);
-    flipCard(cardTwo);
-    cardOne = card;
-    flipCard(cardOne);
-    cardTwo = null;
-  }
-}
-
-function matchPartner() {
-  if(cardOne===null || cardTwo===null) {
-    return
-  }
-
-  if(cardOne.dataset.pair === cardTwo.dataset.pair) {
-
-      cardOne.style.opacity = 0.5;
-      cardTwo.style.opacity = 0.5;
-      cardOne.classList.add("matched");
-      cardTwo.classList.add("matched");
+//create cards for game
+function printRepeatPrevious(x) {
+  let cards = [];
+  for(i=1; i<=x; i++) {
+    let obj = new Object();
+    if(i%2===0) {
+      obj.id=i;
+      obj.content = i-1;
+    } else {
+      obj.id=i;
+      obj.content = i;
       
-      cardOne = null;
-      cardTwo = null;
-  
-
-    // setTimeout(function(){
-    //   cardOne.remove();
-    //   cardTwo.remove();
-    //   }
-    // , 1500);
-    
-    // cardOne.remove();
-    // cardTwo.remove();
-    //https://developer.mozilla.org/pt-BR/docs/Web/API/ChildNode/remove
-    // cardOne.innerHTML = "";
-    // cardTwo.innerHTML = "";
-  }
-
-}
-
-innerCards.forEach(function (currentCard){
-  currentCard.addEventListener('click', function(event) {
-    const card = event.currentTarget;
-
-    setFlipStates(card);
-    
-    matchPartner();
-
-  });
-});
-
-//SNIPPETS TO TAKE A LOOK AGAIN
-
-    // if(cardOne === cardTwo) {
-     
-    //   !flipCard(card);
-    // }
-
-    //if you click the same card already clicked, do nothing
-
-    //Turns two cards face back after click
-
- //Turn back cards
-    // turnBack(document.querySelectorAll(".active"));
-
-    
-function turnBack(activeCards) {
-    if(activeCards.length>=2) {
-      setTimeout(function(cards){
-        for(i=0; i<cards.length; i++) {
-          flipCard(cards[i]);
-        }
-      }, 1500, activeCards);
     }
+    
+  cards.push({obj});
+  }
+  return cards
 }
 
+let cards = printRepeatPrevious(16);
+
+console.log(cards);
+
+//create function to shuffle cards
+//create function to print cards on html(string template/interpolation)
 
 
 
-
-//TODO:
-
-//Game states
-
-// estado inicial - todas as cartas devem estar viradas para baixo
-// clicked_card_1 deve ser null
-// clicked_card_2 deve ser null
-// timerId deve ser null
-
-// ao clicar em uma nova carta ...
-
-// ao clicar em outra nova carta ...
-
-// durante o timeout ...
-
-// ao final do timeout ...
-
-
-// o jogo acaba quando ...?
 
 
